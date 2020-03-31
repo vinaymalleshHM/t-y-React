04757627c532727087325c48adbf672e582b0ab1
vinay <princevinay404@gmail.com>
2020-03-31 22:51:06 +0530

    s

---
+++ React-Projects/react16/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Form from './Form';
+
+function App() {
+  return (
+    <>
+    <p>React Form Validation using React Hooks.</p>
+    
+      <Form/>
+    </>
+  );
+}
+
+export default App;
